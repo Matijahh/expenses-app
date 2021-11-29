edeefb6b2a13fe636bfa8d8f87317a43b1f48657
Matijahh <matijaradofficial@gmail.com>
2021-11-29 18:25:36 +0100

    Using State and Filter Year

--- expenses-app/src/components/expenses/Expenses.js
+++ expenses-app/src/components/expenses/Expenses.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "../filterExpense/ExpenseFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
   const expenses = props.expenses;
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <div className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {expenses.map((item) => {
         return (
           <ExpenseItem
